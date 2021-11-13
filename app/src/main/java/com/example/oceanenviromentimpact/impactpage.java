ff6b81025fdf308f14703769545327114c3235da
blahblah23406 <fasT1432>
2021-11-12 21:34:59 -0800

    working on impact page

--- app/src/main/java/com/example/oceanenviromentimpact/impactpage.java
+++ app/src/main/java/com/example/oceanenviromentimpact/impactpage.java
@@ -3,6 +3,9 @@ package com.example.oceanenviromentimpact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class impactpage extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class impactpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_impactpage);
+        EditText trashremoval = findViewById(R.id.poundsoftrashoutoftheocean);
+        Button enter = findViewById(R.id.enter);
+        TextView marinelifesavecount = findViewById(R.id.marinewildlifesavecount);
+        TextView oceanareasavecount = findViewById(R.id.oceansavedsize);
+        TextView humanpeoplesavecount = findViewById(R.id.humansavedcount);
     }
 }
