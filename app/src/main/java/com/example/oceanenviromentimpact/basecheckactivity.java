169bbdc63b87e963f2cbdfb9495cb9ee48b90ac4
blahblah23406 <fasT1432>
2021-11-12 19:42:26 -0800

    basecheck in progress

---
+++ app/src/main/java/com/example/oceanenviromentimpact/basecheckactivity.java
@@ -0,0 +1,14 @@
+package com.example.oceanenviromentimpact;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basecheckactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basecheckactivity);
+    }
+}
