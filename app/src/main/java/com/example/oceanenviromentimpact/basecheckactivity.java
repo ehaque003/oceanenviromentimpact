38db281eab57468fb24480055e890d00de8159ab
blahblah23406 <fasT1432>
2021-11-12 21:08:07 -0800

    we are working on the backbone of the code

--- app/src/main/java/com/example/oceanenviromentimpact/basecheckactivity.java
+++ app/src/main/java/com/example/oceanenviromentimpact/basecheckactivity.java
@@ -2,13 +2,28 @@ package com.example.oceanenviromentimpact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class basecheckactivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basecheckactivity);
+        EditText input = findViewById(R.id.sizeofthetrashbag);
+        Button done = findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                variable.sizeofthetrashbag = Integer.getInteger(input.getEditableText().toString());
+                Intent intent = new Intent(basecheckactivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/oceanenviromentimpact/variable.java
@@ -0,0 +1,6 @@
+package com.example.oceanenviromentimpact;
+
+public class variable {
+    static int sizeofthetrashbag = 0;
+    static int amountoftrashbefore = 10;
+}
