5c3e669a922dc0ec4992a258ba04b44a21d0e8f4
Ayaan <ayaanzhr11@gmail.com>
2021-11-12 22:37:22 -0800

    backbone done for impact, more improvment needed

--- app/src/main/java/com/example/oceanenviromentimpact/impactpage.java
+++ app/src/main/java/com/example/oceanenviromentimpact/impactpage.java
@@ -3,12 +3,13 @@ package com.example.oceanenviromentimpact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class impactpage extends AppCompatActivity {
-
+    int poundsoftrashinputted = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +19,18 @@ public class impactpage extends AppCompatActivity {
         TextView marinelifesavecount = findViewById(R.id.marinewildlifesavecount);
         TextView oceanareasavecount = findViewById(R.id.oceansavedsize);
         TextView humanpeoplesavecount = findViewById(R.id.humansavedcount);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                poundsoftrashinputted = Integer.getInteger(trashremoval.getEditableText().toString());
+            }
+        });
+        int marinelifeint = Math.round(poundsoftrashinputted/176);
+        marinelifesavecount.setText(marinelifeint);
+        int oceansaveint = Math.round(poundsoftrashinputted);
+        int humansaveint =(int)(poundsoftrashinputted*.5);
+        oceanareasavecount.setText(oceansaveint);
+        humanpeoplesavecount.setText(humansaveint);
+
     }
 }
